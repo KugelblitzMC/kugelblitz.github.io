2dc7a486b3b87f54f854f6754644c2409cdb7059
Pear <20259871+TheRealPear@users.noreply.github.com>
2024-08-12 13:39:09 -0400

    Use recommended workflow with changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kugelblitzmc', // Usually your GitHub org/user name.
   projectName: 'kugelblitzmc.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
